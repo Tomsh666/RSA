b4aa12553cd9e78735cb8e0e8a665b38f19662ee
Tomsh666 <tomshinskiy02@gmail.com>
2024-05-08 08:45:47 +0300

    chore: encryption rework

--- cipher/encrypt.py
+++ cipher/encrypt.py
@@ -1,39 +1,31 @@
-from Crypto.Util.number import getPrime, GCD, inverse
-from Crypto.Util import Padding
+from Crypto.Util import Padding, asn1
 from Crypto.Cipher import AES
 import os
-from dotenv import load_dotenv
+
+# TODO: remake asn1 header
+def add_asn1_header(ciphertext, iv, encrypted_aes_key):
+    asn1_header = asn1.DerSequence([iv, encrypted_aes_key])
+    asn1_header_bytes = asn1_header.encode()
+    encrypted_data_with_header = asn1_header_bytes + iv + ciphertext
+    return encrypted_data_with_header
 
 
-def encrypt():
-    load_dotenv()
-    input_file = os.getenv('INPUT_FILE')
-    output_file = os.getenv('OUTPUT_FILE')
-    with open(input_file, 'r') as f:
+def encrypt(file, n, e, key):
+    input_file = file
+    output_file = "output.bin"
+    with open(input_file, 'rb') as f:
         plain_text = f.read()
-    p = getPrime(1024)
-    q = getPrime(1024)
-    n = p * q
-    phi = (p - 1) * (q - 1)
-
-    e = 2
-    while e < phi:
-        if GCD(e, phi) == 1:
-            break
-        else:
-            e += 1
-    d = inverse(e, phi)
 
     # AES
-    key = os.urandom(32)
     iv = os.urandom(16)
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    padded_text = Padding.pad(plain_text.encode('utf-8'), AES.block_size)
+    padded_text = Padding.pad(plain_text, AES.block_size)
     encrypted_text = cipher.encrypt(padded_text)
-    with open(output_file, 'wb') as f:
-        f.write(iv + encrypted_text)
 
     aes_key = int.from_bytes(key, byteorder='big')
     c = pow(aes_key, e, n)
     encrypted_aes_key = c.to_bytes((n.bit_length() + 7) // 8, byteorder='big')
-    print(encrypted_aes_key)
+
+    encrypted_data_with_header = add_asn1_header(encrypted_text, iv, encrypted_aes_key)
+    with open(output_file, 'wb') as f:
+        f.write(encrypted_data_with_header)
--- main.py
+++ main.py
@@ -1,22 +1,34 @@
+import os
+
 from cipher import encrypt, decrypt
+from Crypto.Util.number import getPrime, GCD
+
 
 def main():
+    p = getPrime(1024)
+    q = getPrime(1024)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 2
+    while e < phi:
+        if GCD(e, phi) == 1:
+            break
+        else:
+            e += 1
     print("1. Encrypt")
     print("2. Decrypt")
     print("3. Sign file")
     print("4. Verify signature")
     print("5. Show parameters")
     choice = input("Select option: ")
-
     if choice == "1":
-        encrypt()
+        aes_key = os.urandom(32)
+        encrypt("input.txt", n, e, aes_key)
     elif choice == "2":
         decrypt()
     elif choice == "3":
         pass
     elif choice == "4":
-        pass
-    elif choice == "5":
         pass
     else:
         print("Wrong choice")
